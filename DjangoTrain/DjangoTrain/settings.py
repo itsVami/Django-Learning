b282c608092e93812c2d58e8063efcebe950216b
itsVami <mohamadsaiedk@gmail.com>
2022-09-11 19:24:40 +0430

    Improving Codes , Adding Account Application For Our Login System , Using Login Dicorator And Mixin

--- DjangoTrain/DjangoTrain/settings.py
+++ DjangoTrain/DjangoTrain/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = "account:home"
+LOGIN_URL = "account:login"
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'account.apps.AccountConfig',
     'extentions',
 ]
 
@@ -57,7 +61,7 @@ ROOT_URLCONF = 'DjangoTrain.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR , 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- DjangoTrain/DjangoTrain/urls.py
+++ DjangoTrain/DjangoTrain/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('blog.urls')),
+    path('account/' , include('account.urls')),
 ]
 
 #DEBUG_MODE
