75104dc22892011cb2aaf5679fdf5786d09bcb79
itsVami <mohamadsaiedk@gmail.com>
2022-09-17 13:55:56 +0430

    AdminLTE Config (Add Style To Add Post's Page With Crispy) , Fix Meta Title Pages

--- DjangoTrain/DjangoTrain/settings.py
+++ DjangoTrain/DjangoTrain/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'account.apps.AccountConfig',
     'extentions',
     'widget_tweaks',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Crispy
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
