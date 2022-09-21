164562f2c2022355010e5de03517efede9e72350
itsVami <mohamadsaiedk@gmail.com>
2022-09-22 00:11:02 +0330

    Fixing Sidebar (Something Like Hover) , Adding Remove Btn In Edinting Article  Page (For Superusers)

--- DjangoTrain/blog/templatetags/base_tags.py
+++ DjangoTrain/blog/templatetags/base_tags.py
@@ -12,3 +12,12 @@ def Category_Navbar ():
     return {
         "category" : Category.objects.filter(Status=True)
     }
+
+@register.inclusion_tag('registration/partials/link.html')
+def link(request , link_name , content):
+    return {
+        "request" : request ,
+        "link_name" : link_name ,
+        "link" : "account:{}".format(link_name) ,
+        "content" : content ,
+    }
