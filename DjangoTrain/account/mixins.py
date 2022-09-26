dc2b25e60e102ce514e13c6e4850e55891c68cb2
itsVami <mohamadsaiedk@gmail.com>
2022-09-27 00:20:33 +0330

    Fix A Bug , Improve Codes

--- DjangoTrain/account/mixins.py
+++ DjangoTrain/account/mixins.py
@@ -49,7 +49,10 @@ class DeleteAccessMixin():
 class AuthorsAccessMixin():
     
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_superuser or request.user.Is_author :
-            return super().dispatch(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            if request.user.is_superuser or request.user.Is_author :
+                return super().dispatch(request, *args, **kwargs)
+            else:
+                return redirect('account:profile')
         else:
-            return redirect('account:profile')
+            return redirect('account:login')
