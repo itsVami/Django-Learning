882f6914607fe5f0e7e30a87bdb6910ca2d6c6fb
itsVami <mohamadsaiedk@gmail.com>
2022-10-08 13:35:13 +0330

    Hot Article's Of Mounth

--- DjangoTrain/blog/templatetags/base_tags.py
+++ DjangoTrain/blog/templatetags/base_tags.py
@@ -4,6 +4,8 @@ from ..models import Category , Article
 from datetime import datetime , timedelta
 from django.db.models import Count , Q
 
+from django.contrib.contenttypes.models import ContentType
+
 register = template.Library()
 
 @register.simple_tag
@@ -16,11 +18,21 @@ def Category_Navbar ():
         "category" : Category.objects.filter(Status=True)
     }
 
-@register.inclusion_tag('blog/partials/popular_article.html')
+@register.inclusion_tag('blog/partials/sidebar_article.html')
 def Popular_Article ():
     last_mounth = datetime.today() - timedelta(days = 30)
     return {
-        "popular_article" : Article.objects.published().annotate(count=Count('hits' , filter= Q(articlehit__created__gt = last_mounth))).order_by('-count' , '-Publish')[:5]
+        "sidebar_article" : Article.objects.published().annotate(count=Count('hits' , filter= Q(articlehit__created__gt = last_mounth))).order_by('-count' , '-Publish')[:5] ,
+        "Title" : 'مقالات پر بازدید' ,
+    }
+
+@register.inclusion_tag('blog/partials/sidebar_article.html')
+def Hot_Article ():
+    last_mounth = datetime.today() - timedelta(days = 30)
+    content_type_ip = ContentType.objects.get(app_label='blog', model='article').id
+    return {
+        "sidebar_article" : Article.objects.published().annotate(count=Count('comments' , filter= Q(comments__posted__gt = last_mounth) and Q(comments__content_type_id = content_type_ip))).order_by('-count' , '-Publish')[:5] ,
+        "Title" : 'مقالات داغ' ,
     }
 
 @register.inclusion_tag('registration/partials/link.html')
